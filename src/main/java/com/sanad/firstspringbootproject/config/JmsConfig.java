20771ab0b649567088340f1a6acb8259c9b2e216
Sanad <Sanad.Herzalla@progressoft.io>
2026-08-11 10:42:12 +0300

    feat: add docker compose included 3 docker containers

--- src/main/java/com/sanad/firstspringbootproject/config/JmsConfig.java
+++ src/main/java/com/sanad/firstspringbootproject/config/JmsConfig.java
@@ -1,6 +1,7 @@
 package com.sanad.firstspringbootproject.config;
 
 import jakarta.jms.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
@@ -9,21 +10,21 @@ import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 @Configuration
 public class JmsConfig {
     @Bean
-    public JmsTemplate topicJmsTemplate(ConnectionFactory connectionFactory) {
+    public JmsTemplate topicJmsTemplate(@Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory) {
         JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
         jmsTemplate.setPubSubDomain(true);
         return jmsTemplate;
     }
 
     @Bean
-    public JmsTemplate queueJmsTemplate(ConnectionFactory connectionFactory) {
+    public JmsTemplate queueJmsTemplate(@Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory) {
         JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
         jmsTemplate.setPubSubDomain(false);
         return jmsTemplate;
     }
 
     @Bean
-    public DefaultJmsListenerContainerFactory topicFactory(ConnectionFactory connectionFactory) {
+    public DefaultJmsListenerContainerFactory topicFactory(@Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setPubSubDomain(true);
